234c46c23f159a9420fb498f6c78864535952fbf
UMer <umer.web2@gmail.com>
2025-07-03 22:56:18 +0500

    update

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const roasts = require('./roasts.json');
+const protectedData = require('./protected.json');
+
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
+// Utility to get a random message from a string or array
+function getProtectedMessage(userId) {
+  const entry = protectedData.users[userId];
+  const messages = entry || protectedData.default;
 
-
+  if (Array.isArray(messages)) {
+    return messages[Math.floor(Math.random() * messages.length)];
+  } else {
+    return messages;
+  }
+}
 
 client.once('ready', () => {
   console.log(`🟢 Logged in as ${client.user.tag}`);
@@ -22,6 +33,14 @@ client.on('messageCreate', message => {
       return;
     }
 
+    // 🔐 Check if the target is protected
+    if (protectedData.users[target.id]) {
+      const response = getProtectedMessage(target.id);
+      message.channel.send(`${message.author}, ${response}`);
+      return;
+    }
+
+    // 🔥 Regular roast
     const roast = roasts[Math.floor(Math.random() * roasts.length)];
     message.channel.send(`${target}, ${roast}`);
   }
